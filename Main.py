ea5616ce4eded3982fa6e27e795b4ec8ed6e7bc5
unknown <john9912@naver.com>
2024-10-31 18:01:26 +0900

    MovieDTO 간단한 설정 및 테스트

--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
+from MovieDTO import MovieData
 from logIn.LogIn import LogIn
 
 def main():
     print("여기서 부터 시작")
+
     
-    # LogIn 클래스 인스턴스 생성
-    login_instance = LogIn()
+    movieData_instance = MovieData() # MovieData 클래스 인스턴스 생성 
+    login_instance = LogIn() # LogIn 클래스 인스턴스 생성
     
+    # MovieData 클래스 내 메서드 호출
+    movieData_instance.load_movie()
+
     # LogIn 클래스 내 메서드 호출
     login_instance.authenticate("조하상", "조하상123")
     login_instance.authenticate("송재현Test", "송재현Test")
--- MovieDTO.py
+++ MovieDTO.py
@@ -1,1 +1,5 @@
 # 영화 데이터 객체 및 메서드 생성 파일
+
+class MovieData:
+    def load_movie(self):
+        print("movie.txt 파일을 로드합니다.")
