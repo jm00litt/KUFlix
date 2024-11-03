d8d30fa3b52a97d2a7aa2889f69cd54657f92490
TaejungKim <tae0803@naver.com>
2024-11-03 20:57:10 +0900

    fix: main 및 movie List 수정

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from MovieDTO import MovieData
-from movie.MovieInfo import display_movie_details 
+from movie.MovieInfo import display_movie_details
 
 def main():
     movieData_instance = MovieData() # MovieData 클래스 인스턴스 생성 및 movie.txt 파일 확인 및 로드
@@ -8,6 +8,6 @@ def main():
     movieData_instance.movies[1]['year'] = 2021
     movieData_instance.update_movieFile()
     
-    # display_movie_details()
+    display_movie_details()
 if __name__ == "__main__":
     main()
