c0ecf0eb486d5683a92c6d5c60a97896c8c0c7ba
MaBeing <john9912@naver.com>
2024-11-04 11:25:55 +0900

    MyPage.py 수정

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from MovieDTO import MovieData
 # from auth.Auth import display_auth_menu
 from movie.MovieList import display_movies_list  # 영화 목록을 표시하는 함수
 from movie.MovieInfo import display_movie_details  # 영화 상세 정보를 표시하는 함수
+from mypage.MyPage import display_myPage
 
 
 def main():
@@ -11,7 +12,7 @@ def main():
 
     if selected_movie_id is not None:
         display_movie_details(selected_movie_id)
-
+1
     # display_auth_menu()
 
 
