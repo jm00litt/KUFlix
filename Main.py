e32dc17a87fe5269fca1738b128698ebb81ef0da
jmlee <lee00litt@gmail.com>
2024-11-05 18:11:19 +0900

    auth에서 바로 home 부르던 걸 main쪽으로 빼서 실행함

--- Main.py
+++ Main.py
@@ -9,7 +9,11 @@ def main():
     MovieData.check_file()  # movie.txt 파일이 존재하는지 확인
     MovieData.load_movie_data()
 
-    display_auth_menu()
+    user_id = display_auth_menu()
+    
+    from home import Home
+    home = Home.Home(user_id) #Home 객체 생성
+    home.home()
 
 if __name__ == "__main__":
     main()
