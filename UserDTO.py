860e87ec88ccd878970ba0a1587793fad7edd071
TaejungKim <tae0803@naver.com>
2024-11-05 19:58:01 +0900

    fix: 이전 수정 반영

--- UserDTO.py
+++ UserDTO.py
@@ -1,18 +1,1 @@
 # 유저 데이터 객체 및 메서드 생성 파일
-# 추후 사용 예정
-
-# def load_user_data(user_id):
-#     # 파일 경로 업데이트
-#     with open("./data/user.txt", "r") as file:
-#         for line in file:
-#             data = line.strip().split("/")
-#             if data[0] == user_id:
-#                 user_info = {
-#                     "id": data[0],
-#                     "password": data[1],
-#                     # 각 리스트와 딕셔너리 데이터 처리 방법
-#                     "favorited_movies": eval(data[2]),
-#                     "rated_movies": eval(data[3])
-#                 }
-#                 return user_info
-#     return None  # 사용자 정보가 파일에 없을 경우 None 반환
--- movie/MovieInfo.py
+++ movie/MovieInfo.py
@@ -2,23 +2,6 @@ from movie.MovieList import get_movies
 from MovieDTO import MovieData
 
 
-def load_user_data(user_id):
-    # 파일 경로 업데이트
-    with open("./data/user.txt", "r") as file:
-        for line in file:
-            data = line.strip().split("/")
-            if data[0] == user_id:
-                user_info = {
-                    "id": data[0],
-                    "password": data[1],
-                    # 각 리스트와 딕셔너리 데이터 처리 방법
-                    "favorited_movies": eval(data[2]),
-                    "rated_movies": eval(data[3])
-                }
-                return user_info
-    return None  # 사용자 정보가 파일에 없을 경우 None 반환
-
-
 def display_movie_details(id,movie_id):
     # 사용자 정보 로드
     user_id = id
@@ -126,8 +109,21 @@ def rate_movie(movie, user_info, movie_id):
         save_user_data(user_info)
         save_movie_data(movie_id, movie)
 
-
-
+def load_user_data(user_id):
+    # 파일 경로 업데이트
+    with open("./data/user.txt", "r") as file:
+        for line in file:
+            data = line.strip().split("/")
+            if data[0] == user_id:
+                user_info = {
+                    "id": data[0],
+                    "password": data[1],
+                    # 각 리스트와 딕셔너리 데이터 처리 방법
+                    "favorited_movies": eval(data[2]),
+                    "rated_movies": eval(data[3])
+                }
+                return user_info
+    return None  # 사용자 정보가 파일에 없을 경우 None 반환
 
 def save_user_data(user_info):
     users = []
