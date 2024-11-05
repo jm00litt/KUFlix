7bc6a08d3802c036d35450f8f9bb937470eece37
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-05 20:40:59 +0900

    fix(Main): import문 상단에 작성 및 Home 객체 생성 삭제

--- Main.py
+++ Main.py
@@ -1,40 +1,33 @@
 from MovieDTO import MovieData
-# from auth.Auth import display_auth_menu
+from home.Home import home
 from movie.MovieList import display_movies_list  # 영화 목록을 표시하는 함수
-from movie.MovieInfo import display_movie_details  # 영화 상세 정보를 표시하는 함수
 from auth.Auth import display_auth_menu
+from search.Search import display_search_page
+from mypage.MyPage import display_my_page
 
 
 def main():
     MovieData.check_file()  # movie.txt 파일이 존재하는지 확인
     MovieData.load_movie_data()
 
-    while True : 
+    while True:
         user_id = display_auth_menu()
-        from home.Home import Home
-        home = Home() #Home 객체 생성
-        home.setUserId(user_id)
-        
-        while True : 
-            selected_number = home.home()
+
+        while True:
+            selected_number = home()
             if selected_number == 0:
                 print('초기화면으로 돌아갑니다.\n')
                 break
             elif selected_number == 1:
                 # MovieList 클래스의 인스턴스를 생성하고 display_movie_list 메서드 호출
-                from movie.MovieList import display_movies_list
-                from movie.MovieInfo import display_movie_details
-                selected_movie_id = display_movies_list()
-                if selected_movie_id is not None:
-                    display_movie_details(home._userId,selected_movie_id)
+                display_movies_list(user_id)
             elif selected_number == 2:
-                from search.Search import display_search_page
-                display_search_page(home._userId)
+                display_search_page(user_id)
             elif selected_number == 3:
-                from mypage.MyPage import display_my_page
-                display_my_page(home._userId)
+                display_my_page(user_id)
             else:
                 print("잘못된 입력입니다. 메뉴 번호를 입력해주세요.")
+
 
 if __name__ == "__main__":
     main()
