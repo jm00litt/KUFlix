8858cb02005421773d4736a86ababeecb76ceabe
jmlee <lee00litt@gmail.com>
2024-12-02 13:01:05 +0900

    바뀐 파일 형식 따라 출력 형식 변경

--- movie/MovieInfo.py
+++ movie/MovieInfo.py
@@ -1,4 +1,5 @@
 from MovieDTO import MovieData
+from auth.Auth import load_user_data as load
 
 def get_movies():
     movie_data = MovieData()
@@ -47,7 +48,7 @@ def display_movie_details(user_id, movie_id):
             print(f"조회수: {movie['views']}")
             print(f"찜: {favorited_status}")
             print("============================================")
-            print("찜하기/해제하기: 1 / 평점 남기기: 2 / 뒤로가기: 0")
+            print("찜하기/해제하기: 1 / 평점 남기기: 2 / 평점 남긴 유저 확인하기: 3 / 뒤로가기: 0")
             if not choose_status(user_id, movie_id):
                 break
     else:
@@ -56,8 +57,8 @@ def display_movie_details(user_id, movie_id):
 def choose_status(user_id, movie_id):
     valid_input = False
     while not valid_input:
-        choice = input("번호를 입력하세요(0-2): ").strip()
-        if choice.isdigit() and int(choice) in [0, 1, 2]:
+        choice = input("번호를 입력하세요(0-3): ").strip()
+        if choice.isdigit() and int(choice) in [0, 1, 2, 3]:
             valid_input = True
         elif not choice.isdigit():
             print("숫자만 입력하세요.")
@@ -69,6 +70,8 @@ def choose_status(user_id, movie_id):
         like_movie(user_id, movie_id)
     elif choice == 2:
         rate_movie(user_id, movie_id)
+    elif choice == 3 :
+        show_rated_users(movie_id)
     elif choice == 0:
         return False
     return True
@@ -126,6 +129,83 @@ def rate_movie(user_id, movie_id):
 
     print(f"평점이 {rating_input}점으로 등록되었습니다!")
 
+
+def show_rated_users(movie_id):
+    while True : 
+        print(f"============================================")
+        users = load()
+        user_ids = users.keys()
+        for i in user_ids :
+            user_data = load_user_data(i)
+            user_movies = user_data['rated_movies'].keys()
+            if movie_id in user_movies:
+                print(f'{user_data["rated_movies"][movie_id]} : {i}')
+        print("============================================")
+        print('유저 아이디 입력시 해당 유저의 <평점,영화> 리스트를 출력합니다.(뒤로가기는 0)\n')
+        valid_input = False
+        while not valid_input:
+            choice = input("아이디 또는 번호를 입력하세요(0): ")
+            if choice.isdigit() and int(choice) in [0]:
+                valid_input = True
+                return
+            elif choice in user_ids:
+                show_users_rate(choice)
+                break
+            else:
+                print("해당 아이디가 없거나 잘못된 번호입니다.")
+
+
+def show_users_rate(user_id):
+    print(f"============================================")
+    print(f'유저 아이디 : {user_id}')
+    rate_total_average = 0
+    rate_total_count = 0
+    rate_genre_average = {
+        "액션" : 0,
+        "코미디" : 0,
+        "로맨스" : 0,
+        "호러" : 0,
+        "SF" : 0}
+    rate_genre_count = {
+        "액션" : 0,
+        "코미디" : 0,
+        "로맨스" : 0,
+        "호러" : 0,
+        "SF" : 0
+        }
+    user_data = load_user_data(user_id)
+    for key in user_data['rated_movies'].keys():
+        movies = get_movies()
+        movie = movies.get(key)
+        genres_str = ','.join(movie['genre'])
+        print(f'<{movie["title"]}> : {user_data["rated_movies"][key]}')
+        print(f"개봉년도: {movie['year']}년")
+        print(f"장르: {genres_str}")
+        print(f"영화 감독: {movie['directors']}")
+        print(f'영화 아이디 : {key}\n')
+        rate_total_count+=1
+        for g in movie['genre'] :
+            rate_genre_count[g]+=1
+            rate_genre_average[g] +=float(user_data["rated_movies"][key])
+        rate_total_average+=float(user_data["rated_movies"][key])
+    if(rate_total_count == 0):
+        print(f'전체 평점 평균 : -')
+    else : 
+        print(f'전체 평점 평균 : {round(rate_total_average/rate_total_count,1)}')
+    print(f'장르별 평점 평균 :')
+    for genre in rate_genre_count.keys():
+        if(rate_genre_count[genre] == 0):
+            print(f'>{genre} : -')
+        else :
+            print(f'>{genre} : {round(rate_genre_average[genre]/rate_genre_count[genre],1)}')
+    print(f"============================================")
+    while True :
+        print('뒤로가기 : 0')
+        zero = input('번호를 입력하세요 : ')
+        if zero == '0':
+            return
+        else :
+            print('잘못된 입력입니다.')
     
 def load_user_data(user_id):
     with open("./data/user.txt", "r") as file:
